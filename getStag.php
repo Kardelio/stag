76d862c34332f253c4f56e9fba264c769d115c59
kardelio <kardeliogithub@outlook.com>
2019-06-26 15:54:03 +0100

    Fixed the jenk-stag issue regarding shortlog not working due to missing HEAD reference, also added cron script

--- getStag.php
+++ getStag.php
@@ -1,6 +1,8 @@
 <?php
-    $branch = $_POST["branch"];
     $os = $_POST["os"];
-    $output = shell_exec('./jenk-stag '.$branch. ' '.$os);
-    echo $output;
+	if(strpos($os,'android') !== false){
+		echo file_get_contents("/Users/automation/emar-myvery_android.stag.json");
+	} else {
+		echo file_get_contents("/Users/automation/emar-myvery_ios.stag.json");
+	}
 ?>
